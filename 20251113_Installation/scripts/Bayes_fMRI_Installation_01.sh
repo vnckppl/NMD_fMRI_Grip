91e081e36fcbb6c6c1410b59ada5494044d662af
vnckppl <vincent.koppelmans@utah.edu>
2025-11-13 18:37:53 -0700

    Installation scripts

---
+++ 20251113_Installation/scripts/Bayes_fMRI_Installation_01.sh
@@ -0,0 +1,11 @@
+# * Install prerequisites for compiling BayesfMRI in R
+
+# ** For 'sf'
+sudo apt-get install -y \
+     libudunits2-dev \
+     libgdal-dev \
+     libgeos-dev \
+     libproj-dev
+
+# ** For 'excursions'
+sudo apt-get install -y libgsl-dev
---
+++ 20251113_Installation/scripts/Bayes_fMRI_Installation_02.R
@@ -0,0 +1,20 @@
+## * Install packages
+# sf and excursions are prerquisites for BayesfMRI
+install.packages(c("sf", "excursions", "BayesfMRI"))
+
+## * Install INLA
+install.packages("INLA",
+                 repos = c(getOption("repos"),
+                           INLA = "https://inla.r-inla-download.org/R/stable"),
+                 dep = FALSE
+                 )
+
+## * Load Libraries
+library("ciftiTools")
+library("BayesfMRI")
+library("INLA")
+
+## * Set Workbench installation location for ciftiTools
+ciftiTools.setOption(
+  "wb_path","~/Software/workbench/2.1.0/bin_linux64"
+)
